d93ff68e2ba6e50badc0e86c55c8628307865f1d
Vadim <vadik.mahnev@yandex.ru>
2026-09-06 18:56:23 +0300

    add: test and logic

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	for {
+		var cmd string
+		fmt.Println("Введите команду: ")
+		_, err := fmt.Scanln(&cmd)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err = f(cmd); err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		fmt.Println("Вы выиграли | Ничья")
+	}
+}

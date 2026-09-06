f16d72aadcfbd87d90284d5939782ad73436c689
krystaltechz <daning657@mail.ru>
2026-09-06 17:17:21 +0300

    main.go

--- main.go
+++ main.go
@@ -1,14 +1,1 @@
 package main
-
-import (
-	"errors"
-	"fmt"
-	"log/slog"
-)
-
-func main() {
-	fmt.Println("Привет, мир!")
-	println("Hello, PTU")
-	print("Hello")
-	slog.Error("Hello bot", "error", errors.New("hello"))
-}
